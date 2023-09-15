f9ae30137cf66533c7d61011987d352cc099cd5c
Adityaapandkar <adityaapandkar11@gmail.com>
2023-09-15 16:34:45 +0530

    Login initial

--- my-app/src/Components/Login.jsx
+++ my-app/src/Components/Login.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Row from "react-bootstrap/Row";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <>
-    <div>Login</div>
-    </>
-  )
-}
+      <div>
+        <Form>
+          <Row className="mb-3">
+            <Form.Group as={Col} md="4" controlId="validationCustomEmail">
+              <Form.Label>Email</Form.Label>
+              <InputGroup hasValidation>
+                <Form.Control
+                  type="text"
+                  placeholder="Email"
+                  aria-describedby="inputGroupPrepend"
+                  required
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please choose an email.
+                </Form.Control.Feedback>
+              </InputGroup>
+            </Form.Group>
+          </Row>
 
-export default Login
+          <Row className="mb-3">
+            <Form.Group as={Col} md="4" controlId="validationCustomPassword">
+              <Form.Label>Password</Form.Label>
+              <InputGroup hasValidation>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  aria-describedby="inputGroupPrepend"
+                  required
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please choose a Password.
+                </Form.Control.Feedback>
+              </InputGroup>
+            </Form.Group>
+          </Row>
+        </Form>
+
+        <Button type="submit">Next</Button>
+
+        <p>
+          Don't have an account? <Link to="/signup1">Sign Up</Link>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Login;
