5e13d1ed9672ca4ad06cef1f5192b8a81f45e40c
Huzefa Dohadwala <dohadwalahuzefa@gmail.com>
2023-09-15 19:53:28 +0530

    sub routes

--- my-app/src/Components/StudentDashboard.jsx
+++ my-app/src/Components/StudentDashboard.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { Route, Routes } from "react-router-dom";
 import Sidebar from './Sidebar';
+import Applied from './Applied';
+import VisaNPrep from './VisaNPrep';
 
 const StudentDashboard = () => {
   return (
     <div className="flex gap-0">
       {/* Sidebar (1/4 width) */}
-        <Sidebar />
+      <Sidebar />
       {/* Main Content (3/4 width) */}
       <div className="w-full bg-gray-400 p-4">
-        {/* Your main content goes here */}
-        <div className="std_dash">Main Content</div>
+        <Routes>
+          <Route path="applied" element={<Applied/>} />
+          <Route path="analytics" element={<Applied/>} />
+          <Route path="visanprep" element={<VisaNPrep/>} />
+        </Routes>
       </div>
     </div>
   );
--- my-app/src/Routes/MainRoutes.js
+++ my-app/src/Routes/MainRoutes.js
@@ -5,8 +5,6 @@ import Login from "../Components/Login";
 import SignUp1 from "../Components/SignUp1";
 import SignUp2 from "../Components/SignUp2";
 import StudentDashboard from "../Components/StudentDashboard";
-import Applied from "../Components/Applied";
-import VisaNPrep from "../Components/VisaNPrep";
 
 function MainRoutes() {
   return (
@@ -15,10 +13,7 @@ function MainRoutes() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup1" element={<SignUp1 />} />
       <Route path="/signup2" element={<SignUp2 />} />
-      <Route path="/studentdashboard" element={<StudentDashboard/>} />
-      <Route path="/studentdashboard/applied" element={<Applied/>}/>
-      <Route path="/studentdashboard/analytics" element={<Applied/>}/>
-      <Route path="/studentdashboard/visanprep" element={<VisaNPrep/>}/>
+      <Route path="/studentdashboard/*" element={<StudentDashboard />} />
       <Route path="/" element={<Home />} />
     </Routes>
   );
